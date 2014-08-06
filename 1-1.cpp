24c1307603224c8ac762759b028f71f31143542a
Leandro Lovisolo <leandro@leandro.me>
2014-08-05 23:00:23 -0300

    Initial import

---
+++ 1-1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	if(argc != 1) return -1;
+	char occurrences[256];
+	for(int i = 0; i < 256; i++) occurrences[i] = 0;
+	char *str = argv[1];
+	while(str != 0) {
+		occurrences[str]++;
+		str++;
+	}
+	for(i = 0; i < 256; i++) {
+		if(occurrences[i] > 1) {
+			cout << "Not all characters unique." << endl;
+			return 0;
+		}
+	}
+	cout << "All characters unique." << endl;
+	return 0;
+}
---
+++ Makefile
@@ -0,0 +1,8 @@
+CPP = g++
+CPPFLAGS = -o
+CC = $(CPP) $(CPPFLAGS)
+
+all: 1-1
+
+1-1: 1-1.cpp
+	$(CC) 1-1 1-1.cpp
