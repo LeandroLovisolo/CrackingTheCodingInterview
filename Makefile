9d6fb74e80a2e2c2ca992a31d710b70acb2be331
Leandro Lovisolo <leandro@leandro.me>
2014-08-06 07:04:40 -0300

    Problem 1.8

--- Makefile
+++ Makefile
@@ -9,7 +9,8 @@ ALL = bin/1.1.1 \
       bin/1.4   \
       bin/1.5   \
       bin/1.6   \
-      bin/1.7
+      bin/1.7   \
+      bin/1.8
 
 all: $(ALL)
 
---
+++ src/1.8.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool is_rotation(const string &s, const string &t) {
+	string u;
+	u += s;
+	u += s;
+	return u.find(t) != string::npos;
+}
+
+int main(int argc, char **argv) {
+	if(argc != 3) return -1;
+	string s(argv[1]);
+	string t(argv[2]);
+	if(is_rotation(s, t)) {
+		cout << "One is a rotation of the other." << endl;
+	} else {
+		cout << "The strings are not rotations." << endl;
+	}
+	return 0;
+}
